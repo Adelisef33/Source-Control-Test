8b72a691295057dcd174066748672f48001ffe24
Adelisef33 <AdeliseFerris@gmail.com>
2025-07-08 21:23:24 -0700

    Implemented the method that asks the user for their name and then returns a greeting with the user's name

--- Source Control Test/Program.cs
+++ Source Control Test/Program.cs
@@ -7,7 +7,14 @@ static void Main()
     NumberGuesser();
 }
 
-//Going to implement a method that takes a name and prints a greeting
+//Method to greet the user by their name
+static void HelloName()
+{
+    Console.WriteLine("What is your name?");
+    string name = Console.ReadLine();
+
+    Console.WriteLine($"Hello {name}, I hope you are having an amazing day!");
+}
 
 
 // Going to implement a method that takes input from the user to guess a number
