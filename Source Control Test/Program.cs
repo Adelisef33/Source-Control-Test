54938c00f740d5a2d767bb857ae051016445bc11
Adelisef33 <AdeliseFerris@gmail.com>
2025-07-08 21:02:47 -0700

    Add project files.

---
+++ Source Control Test/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Why Hello World! How are you today?");
+
+static void Main()
+{
+    HelloName();
+
+    NumberGuesser();
+}
+
+//Going to implement a method that takes a name and prints a greeting
+
+
+// Going to implement a method that takes input from the user to guess a number
+
+
